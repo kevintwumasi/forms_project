fe9d2c4526fb5cf8b9863d6066298855ba3ccd69
Jamie Patrick Ching <jamiepatrick.ching@ucalgary.ca>
2019-10-28 13:52:17 -0600

    a

---
+++ testing_Data/Data_Structs/src/Data_DateDouble.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+public class Data_DateDouble extends Data_Date
+{
+	private ArrayList<Double> EntryArray = new ArrayList<Double>();
+	// constructor
+	public Data_DateDouble(String Type) {
+		super(Type);
+	}
+	public Data_DateDouble(String Type, String Units) {
+		super(Type, Units);
+	}
+	// get methods
+	public double GetData_Entry(int choice) {
+		return this.EntryArray.get(choice);
+	}
+
+	public String GetData_Line(int choice) {
+		return (GetData_Date(choice) + "\t" + GetData_Entry(choice) + " " + GetData_Units() + "\n");
+	}
+
+	public String GetData() {
+		int i;
+		String ArrayStringed = "";
+		for (i = 0; i < EntryArray.size(); ++i) {
+			ArrayStringed += GetData_Line(i);
+		}
+		return ArrayStringed;
+	}
+
+	public void SetData_Entry(double Data_Entry) {
+		this.EntryArray.add(Data_Entry);
+		SetData_DateEntry();
+	}
+}
---
+++ testing_Data/Data_Structs/src/User/User.java
@@ -0,0 +1,18 @@
+public class User
+{
+	private Info info;
+	private Gender gender;
+	private Height height;
+	private Weight weight;
+	private BF bf;
+	private Activity_Level activity_level;
+	private Steps steps;
+	private Resting_BPM resting_bpm;
+	private Benches benches;
+	private Deadlifts deadlifts;
+	private Squats squats;
+
+	public User() {
+		;
+	}
+}
